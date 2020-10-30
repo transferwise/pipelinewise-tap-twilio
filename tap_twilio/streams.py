b294b5d696e940b76e7a6aceb56f19d76b0f6d91
Eric Josafat Pulido Padilla <eric.pulido@transferwise.com>
2020-10-30 17:01:01 +0200

    Testing Taskrouter streams and debugging sync script.

--- tap_twilio/streams.py
+++ tap_twilio/streams.py
@@ -15,310 +15,53 @@
 
 STREAMS = {
     # Reference: https://www.twilio.com/docs/usage/api/account#read-multiple-account-resources
-    'accounts': {
-        'api_url': 'https://api.twilio.com',
-        'api_version': '2010-04-01',
-        'path': 'Accounts.json',
-        'data_key': 'accounts',
-        'key_properties': ['sid'],
-        'replication_method': 'FULL_TABLE',  # Fetch ALL, filter results
-        'replication_keys': ['date_updated'],
-        'params': {},
-        'pagingation': 'root',
-        'children': {
-            # pylint: disable=line-too-long
-            # Reference: https://www.twilio.com/docs/usage/api/address#read-multiple-address-resources
-            'addresses': {
-                'api_url': 'https://api.twilio.com',
-                'api_version': '2010-04-01',
-                'path': 'Accounts/{ParentId}/Addresses.json',
-                'data_key': 'addresses',
-                'key_properties': ['sid'],
-                'replication_method': 'INCREMENTAL',  # Fetch ALL, filter results
-                'replication_keys': ['date_updated'],
-                'params': {},
-                'pagingation': 'root',
-                'children': {
-                    # pylint: disable=line-too-long
-                    # Reference: https://www.twilio.com/docs/usage/api/address?code-sample=code-list-dependent-pns-subresources&code-language=curl&code-sdk-version=json#instance-subresources
-                    'dependent_phone_numbers': {
-                        'api_url': 'https://api.twilio.com',
-                        'api_version': '2010-04-01',
-                        'path': 'Accounts/{ParentId}/Addresses/{ParentId}/DependentPhoneNumbers.json',
-                        'data_key': 'dependent_phone_numbers',
-                        'key_properties': ['sid'],
-                        'replication_method': 'FULL_TABLE',  # ALL for parent Address
-                        'params': {},
-                        'pagingation': 'root',
-                        'parent': 'address'
-                    }
-                }
-            },
-            # pylint: disable=line-too-long
-            # Reference: https://www.twilio.com/docs/usage/api/applications#read-multiple-application-resources
-            'applications': {
-                'api_url': 'https://api.twilio.com',
-                'api_version': '2010-04-01',
-                'path': 'Accounts/{ParentId}/Applications.json',
-                'data_key': 'applications',
-                'key_properties': ['sid'],
-                'replication_method': 'INCREMENTAL',  # Fetch ALL, filter results
-                'replication_keys': ['date_updated'],
-                'params': {},
-                'pagingation': 'root'
-            },
-            # pylint: disable=line-too-long
-            # Reference: https://www.twilio.com/docs/phone-numbers/api/availablephonenumber-resource#read-a-list-of-countries
-            'available_phone_number_countries': {
-                'api_url': 'https://api.twilio.com',
-                'api_version': '2010-04-01',
-                'path': 'Accounts/{ParentId}/AvailablePhoneNumbers.json',
-                'data_key': 'countries',
-                'key_properties': ['country_code'],
-                'replication_method': 'FULL_TABLE',
-                'params': {},
-                'pagingation': 'none',
-                'children': {
-                    # pylint: disable=line-too-long
-                    # Reference: https://www.twilio.com/docs/phone-numbers/api/availablephonenumberlocal-resource#read-multiple-availablephonenumberlocal-resources
-                    'available_phone_numbers_local': {
-                        'api_url': 'https://api.twilio.com',
-                        'api_version': '2010-04-01',
-                        'path': 'Accounts/{AccountSid}/AvailablePhoneNumbers/{ParentId}/Local.json',
-                        'data_key': 'available_phone_numbers',
-                        'key_properties': ['iso_country', 'phone_number'],
-                        'replication_method': 'FULL_TABLE',  # ALL for parent Address
-                        'params': {},
-                        'pagingation': 'root',
-                        'activate_version': True
-                    },
-                    # pylint: disable=line-too-long
-                    # Reference: https://www.twilio.com/docs/phone-numbers/api/availablephonenumber-mobile-resource#read-multiple-availablephonenumbermobile-resources
-                    'available_phone_numbers_mobile': {
-                        'api_url': 'https://api.twilio.com',
-                        'api_version': '2010-04-01',
-                        'path': 'Accounts/{AccountSid}/AvailablePhoneNumbers/{ParentId}/Mobile.json',
-                        'data_key': 'available_phone_numbers',
-                        'key_properties': ['iso_country', 'phone_number'],
-                        'replication_method': 'FULL_TABLE',  # ALL for parent Address
-                        'params': {},
-                        'pagingation': 'root',
-                        'activate_version': True
-                    },
-                    # pylint: disable=line-too-long
-                    # Reference: https://www.twilio.com/docs/phone-numbers/api/availablephonenumber-tollfree-resource#read-multiple-availablephonenumbertollfree-resources
-                    'available_phone_numbers_toll_free': {
-                        'api_url': 'https://api.twilio.com',
-                        'api_version': '2010-04-01',
-                        'path': 'Accounts/{AccountSid}/AvailablePhoneNumbers/{ParentId}/TollFree.json',
-                        'data_key': 'available_phone_numbers',
-                        'key_properties': ['iso_country', 'phone_number'],
-                        'replication_method': 'FULL_TABLE',  # ALL for parent Address
-                        'params': {},
-                        'pagingation': 'root',
-                        'activate_version': True
-                    }
-                }
-            },
-            # pylint: disable=line-too-long
-            # Reference: https://www.twilio.com/docs/phone-numbers/api/incomingphonenumber-resource#read-multiple-incomingphonenumber-resources
-            'incoming_phone_numbers': {
-                'api_url': 'https://api.twilio.com',
-                'api_version': '2010-04-01',
-                'path': 'Accounts/{ParentId}/IncomingPhoneNumbers.json',
-                'data_key': 'incoming_phone_numbers',
-                'key_properties': ['sid'],
-                'replication_method': 'INCREMENTAL',  # Fetch ALL, filter results
-                'replication_keys': ['date_updated'],
-                'params': {},
-                'pagingation': 'none'
-            },
-            # Reference: https://www.twilio.com/docs/usage/api/keys#read-a-key-resource
-            'keys': {
-                'api_url': 'https://api.twilio.com',
-                'api_version': '2010-04-01',
-                'path': 'Accounts/{ParentId}/Keys.json',
-                'data_key': 'keys',
-                'key_properties': ['sid'],
-                'replication_method': 'INCREMENTAL',  # Fetch ALL, filter results
-                'replication_keys': ['date_updated'],
-                'params': {},
-                'pagingation': 'root'
-            },
-            # pylint: disable=line-too-long
-            # Reference: https://www.twilio.com/docs/sms/api/message-resource#read-multiple-message-resources
-            'calls': {
-                'api_url': 'https://api.twilio.com',
-                'api_version': '2010-04-01',
-                'path': 'Accounts/{ParentId}/Calls.json',
-                'data_key': 'calls',
-                'key_properties': ['sid'],
-                'replication_method': 'INCREMENTAL',  # Filter query
-                'replication_keys': ['end_time'],
-                'bookmark_query_field_from': 'EndTime>',  # Daily
-                'bookmark_query_field_to': 'EndTime<',
-                'params': {},
-                'pagingation': 'root'
-            },
-            # pylint: disable=line-too-long
-            # Reference: https://www.twilio.com/docs/voice/api/conference-resource#read-multiple-conference-resources
-            'conferences': {
-                'api_url': 'https://api.twilio.com',
-                'api_version': '2010-04-01',
-                'path': 'Accounts/{ParentId}/Conferences.json',
-                'data_key': 'conferences',
-                'key_properties': ['sid'],
-                'replication_method': 'INCREMENTAL',  # Filter query
-                'replication_keys': ['date_updated'],
-                'bookmark_query_field_from': 'DateUpdated>',  # Daily
-                'bookmark_query_field_to': 'DateUpdated<',
-                'params': {},
-                'pagingation': 'root',
-                'children': {
-                    # pylint: disable=line-too-long
-                    # Reference: https://www.twilio.com/docs/voice/api/conference-participant-resource#read-multiple-participant-resources
-                    'conference_participants': {
-                        'api_url': 'https://api.twilio.com',
-                        'api_version': '2010-04-01',
-                        'path': 'Accounts/{ParentId}/Conferences/{ParentId}/Participants.json',
-                        'data_key': 'participants',
-                        'key_properties': ['uri'],
-                        'replication_method': 'FULL_TABLE',  # ALL for parent Conference
-                        'params': {},
-                        'pagingation': 'root'
-                    }
-                }
-            },
-            # pylint: disable=line-too-long
-            # Reference: https://www.twilio.com/docs/voice/api/outgoing-caller-ids#outgoingcallerids-list-resource
-            'outgoing_caller_ids': {
-                'api_url': 'https://api.twilio.com',
-                'api_version': '2010-04-01',
-                'path': 'Accounts/{ParentId}/OutgoingCallerIds.json',
-                'data_key': 'outgoing_caller_ids',
-                'key_properties': ['sid'],
-                'replication_method': 'INCREMENTAL',  # Fetch ALL, filter results
-                'replication_keys': ['date_updated'],
-                'params': {},
-                'pagingation': 'none'
-            },
-            # pylint: disable=line-too-long
-            # Reference: https://www.twilio.com/docs/voice/api/recording#read-multiple-recording-resources
-            'recordings': {
-                'api_url': 'https://api.twilio.com',
-                'api_version': '2010-04-01',
-                'path': 'Accounts/{ParentId}/Recordings.json',
-                'data_key': 'recordings',
-                'key_properties': ['sid'],
-                'replication_method': 'INCREMENTAL',  # Filter query
-                'replication_keys': ['date_created'],
-                'bookmark_query_field_from': 'DateCreated>',  # Daily
-                'bookmark_query_field_to': 'DateCreated<',
-                'params': {},
-                'pagingation': 'root'
-            },
-            # pylint: disable=line-too-long
-            # Reference: https://www.twilio.com/docs/voice/api/recording-transcription?code-sample=code-read-list-all-transcriptions&code-language=curl&code-sdk-version=json#read-multiple-transcription-resources
-            'transcriptions': {
-                'api_url': 'https://api.twilio.com',
-                'api_version': '2010-04-01',
-                'path': 'Accounts/{ParentId}/Transcriptions.json',
-                'data_key': 'transcriptions',
-                'key_properties': ['sid'],
-                'replication_method': 'INCREMENTAL',  # Fetch ALL, filter results
-                'replication_keys': ['date_updated'],
-                'params': {},
-                'pagingation': 'root'
-            },
-            # pylint: disable=line-too-long
-            # Reference: https://www.twilio.com/docs/voice/api/queue-resource#read-multiple-queue-resources
-            'queues': {
-                'api_url': 'https://api.twilio.com',
-                'api_version': '2010-04-01',
-                'path': 'Accounts/{ParentId}/Queues.json',
-                'data_key': 'queues',
-                'key_properties': ['sid'],
-                'replication_method': 'INCREMENTAL',  # Fetch ALL, filter results
-                'replication_keys': ['date_updated'],
-                'params': {},
-                'pagingation': 'root'
-            },
-            # pylint: disable=line-too-long
-            # Reference: https://www.twilio.com/docs/sms/api/message-resource#read-multiple-message-resources
-            'messages': {
-                'api_url': 'https://api.twilio.com',
-                'api_version': '2010-04-01',
-                'path': 'Accounts/{ParentId}/Messages.json',
-                'data_key': 'messages',
-                'key_properties': ['sid'],
-                'replication_method': 'INCREMENTAL',  # Filter query
-                'replication_keys': ['date_sent'],
-                'bookmark_query_field_from': 'DateSent>',  # Daily
-                'bookmark_query_field_to': 'DateSent<',
-                'params': {},
-                'pagingation': 'root',
-                'children': {
-                    # pylint: disable=line-too-long
-                    # Reference: https://www.twilio.com/docs/sms/api/media-resource#read-multiple-media-resources
-                    'message_media': {
-                        'api_url': 'https://api.twilio.com',
-                        'api_version': '2010-04-01',
-                        'path': 'Accounts/{AccountSid}/Messages/{ParentId}/Media.json',
-                        'data_key': 'media_list',
-                        'key_properties': ['sid'],
-                        'replication_method': 'FULL_TABLE',  # ALL for parent Address
-                        'params': {},
-                        'pagingation': 'root'
-                    }
-                }
-            },
-            # pylint: disable=line-too-long
-            # Reference: https://www.twilio.com/docs/usage/api/usage-record#read-multiple-usagerecord-resources
-            'usage_records': {
-                'api_url': 'https://api.twilio.com',
-                'api_version': '2010-04-01',
-                'path': 'Accounts/{ParentId}/Usage/Records.json',
-                'data_key': 'usage_records',
-                'key_properties': ['account_sid', 'category', 'start_date'],
-                'replication_method': 'INCREMENTAL',  # Filter query
-                'replication_keys': ['end_date'],
-                'bookmark_query_field_from': 'StartDate',  # Daily
-                'bookmark_query_field_to': 'EndDate',
-                'params': {},
-                'pagingation': 'root'
-            },
-            # pylint: disable=line-too-long
-            # Reference: https://www.twilio.com/docs/usage/api/usage-trigger#read-multiple-usagetrigger-resources
-            'usage_triggers': {
-                'api_url': 'https://api.twilio.com',
-                'api_version': '2010-04-01',
-                'path': 'Accounts/{ParentId}/Usage/Triggers.json',
-                'data_key': 'usage_triggers',
-                'key_properties': ['sid'],
-                'replication_method': 'INCREMENTAL',  # Fetch ALL, filter results
-                'replication_keys': ['date_updated'],
-                'params': {},
-                'pagingation': 'root'
-            },
 
-        }
-    },
-    # pylint: disable=line-too-long
-    # Reference: https://www.twilio.com/docs/usage/monitor-alert#read-multiple-alert-resources
-    'alerts': {
-        'api_url': 'https://monitor.twilio.com',
+    'workspaces': {
+        'api_url': 'https://taskrouter.twilio.com',
         'api_version': 'v1',
-        'path': 'Alerts',
-        'data_key': 'alerts',
+        'path': 'Workspaces',
+        'data_key': 'workspaces',
         'key_properties': ['sid'],
-        'replication_method': 'INCREMENTAL',  # Filter query
+        'replication_method': 'FULL_TABLE',
         'replication_keys': ['date_updated'],
-        'bookmark_query_field_from': 'StartDate',  # Bookmark
-        'bookmark_query_field_to': 'EndDate',  # Current Date
-        'max_days_ago': 30,
         'params': {},
-        'pagingation': 'meta'
-    }
+        'pagination': 'root',
+        'children': {
+            'activities': {
+                'api_url': '',  # https://taskrouter.twilio.com',
+                'api_version': 'v1',
+                'path': 'Workspaces/{ParentId}/Activities',
+                'data_key': 'activities',
+                'key_properties': ['sid'],
+                'replication_method': 'FULL_TABLE',
+                'replication_keys': ['date_updated'],
+                'params': {},
+                'pagination': 'root',
+            },
+            'workers': {
+                'api_url': '',  # https://taskrouter.twilio.com',
+                'api_version': 'v1',
+                'path': 'Workspaces/{ParentId}/Workers',
+                'data_key': 'workers',
+                'key_properties': ['sid'],
+                'replication_method': 'FULL_TABLE',
+                'replication_keys': ['date_updated'],
+                'params': {},
+                'pagination': 'none',
+            },
+            'workflows': {
+                'api_url': '',#https://taskrouter.twilio.com',
+                'api_version': 'v1',
+                'path': 'Workspaces/{ParentId}/Workflows',
+                'data_key': 'workflows',
+                'key_properties': ['sid'],
+                'replication_method': 'FULL_TABLE',
+                'replication_keys': ['date_updated'],
+                'params': {},
+                'pagination': 'root',
+            },
+        },
+    },
 }
 
 
