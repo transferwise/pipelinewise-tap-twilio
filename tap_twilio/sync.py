be844168da883bc40ccdf347cfdbedcfef1f2b57
Mark Poole <mpoole@apache.org>
2021-06-22 17:58:17 -0700

    .

--- tap_twilio/sync.py
+++ tap_twilio/sync.py
@@ -335,7 +335,8 @@ def sync_endpoint(
                                 if id_field == 'id':
                                     parent_id_field = id_field
                                 i = i + 1
-                            parent_id = record.get(parent_id_field)
+                            if record.get(parent_id_field):
+                                parent_id = record.get(parent_id_field)
 
                             # sync_endpoint for child
                             LOGGER.info(
